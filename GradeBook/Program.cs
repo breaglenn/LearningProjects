6040e018b71baca6cacf86d57a0bdc93a8cb410d
breaglenn <53768392+breaglenn@users.noreply.github.com>
2019-10-01 08:24:58 -0400

    000000 - finished module

--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -1,21 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
 
+
 namespace GradeBook
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var book = new Book("Scott's Grade Book");
-            book.AddGrade(89.1);
-            book.AddGrade(90.5);
-            book.AddGrade(77.5);
-            book.ShowStatistics();
+            var book = new DiskBook("Scott's Grade Book");
+            book.GradeAdded += OnGradeAdded;
 
-                        
-            
+            EnterGrades(book);
 
+            var stats = book.GetStatistics();
+
+            Console.WriteLine($"For the book named {book.Name}");
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The higheest grade is {stats.High}");
+            Console.WriteLine($"The average grade is {stats.Average:N1}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
+
+
+
+
+        }
+
+        private static void EnterGrades(IBook book)
+        {
+            while (true)
+            {
+                Console.WriteLine("Please enter a grade. Enter q to quit.");
+                var input = Console.ReadLine();
+
+                if (input == "q")
+                {
+                    break;
+                }
+
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("**");
+                }
+            }
+        }
+
+        static void OnGradeAdded(object sender, EventArgs e)
+        {
+            Console.WriteLine("A grade was added.");
         }
     }
 }
---
+++ GradeBook/TypeTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace GradeBook.Tests
+{
+
+    public delegate string WriteLogDelegate(string logMessage);
+        
+    public class TypeTests
+    {
+        int count = 0;
+
+        [Fact]
+        public void WriteLogDelegateCanPointToMethod()
+        {
+            WriteLogDelegate log = ReturnMessage;
+
+            log += ReturnMessage;
+            log += IncrementCount;
+
+            var result = log("Hello!");
+            //Assert.Equal(3, count);
+        }
+
+        string IncrementCount(string message)
+        {
+            count++;
+            return message.ToLower();
+        }
+        string ReturnMessage(string message)
+        {
+            count++;
+            return message;
+        }
+
+        //[Fact]
+        //public void ValueTypesAlsoPassByValue()
+        //{
+            
+        //    SetInt(ref x);
+
+        //    //Assert.Equal(42, x);
+        //}
+
+        private void SetInt(ref Int32 z)
+        {
+            z = 42;
+        }
+
+        
+    }
+
+    internal class Assert: Attribute
+    {
+    }
+
+    internal class FactAttribute : Attribute
+    {
+    }
+}
