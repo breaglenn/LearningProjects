adae9259de6e735c81ca424ab688abc92d6677ec
Brea <brea.glenn@morleynet.com>
2020-03-02 11:50:59 -0500

    03022020 - finished

--- WebApiFundamentals-master/WebApiFundamentals-master/starting/TheCodeCamp/App_Start/WebApiConfig.cs
+++ WebApiFundamentals-master/WebApiFundamentals-master/starting/TheCodeCamp/App_Start/WebApiConfig.cs
@@ -1,8 +1,11 @@
-﻿using Newtonsoft.Json.Serialization;
+﻿using Microsoft.Web.Http.Versioning;
+using Microsoft.Web.Http.Versioning.Conventions;
+using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using TheCodeCamp.Controllers;
 
 namespace TheCodeCamp
 {
@@ -12,6 +15,23 @@ namespace TheCodeCamp
     {
       // Web API configuration and services
       AutofacConfig.Register();
+
+            config.AddApiVersioning(cfg =>
+            {
+                cfg.DefaultApiVersion = new Microsoft.Web.Http.ApiVersion(1, 1);
+                cfg.AssumeDefaultVersionWhenUnspecified = true;
+                cfg.ReportApiVersions = true;
+                cfg.ApiVersionReader = //new UrlSegmentApiVersionReader();
+                ApiVersionReader.Combine(
+                    new HeaderApiVersionReader("X-Version"),
+                    new QueryStringApiVersionReader("ver"));
+
+                cfg.Conventions.Controller<TalksController>()
+                .HasApiVersion(1, 0)
+                .HasApiVersion(1, 1)
+                .Action(mbox => mbox.Get(default(string), default(int), default(bool)))
+                    .MapToApiVersion(2, 0);
+            });
 
         // Change Case of JSON
         config.Formatters.JsonFormatter.SerializerSettings.ContractResolver =
